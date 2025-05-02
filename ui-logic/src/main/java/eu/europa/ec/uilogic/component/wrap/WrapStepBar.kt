2592a6d447694a48cd2343a063a0332825a16bd5
Paulina Strychacz <paulina.strychacz@t-systems.com>
2025-05-02 13:00:20 +0200

    Make the top step bar scrollable

--- ui-logic/src/main/java/eu/europa/ec/uilogic/component/wrap/WrapStepBar.kt
+++ ui-logic/src/main/java/eu/europa/ec/uilogic/component/wrap/WrapStepBar.kt
@@ -18,52 +18,147 @@ package eu.europa.ec.uilogic.component.wrap
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import eu.europa.ec.uilogic.component.preview.PreviewTheme
-import eu.europa.ec.uilogic.component.preview.ThemeModePreviews
 import eu.europa.ec.uilogic.component.utils.SIZE_EXTRA_SMALL
 import eu.europa.ec.uilogic.component.utils.SPACING_EXTRA_SMALL
 import eu.europa.ec.uilogic.component.utils.SPACING_SMALL_PLUS
+import kotlinx.coroutines.launch
+import androidx.compose.ui.text.TextMeasurer
+import androidx.compose.ui.text.rememberTextMeasurer
+import androidx.compose.ui.tooling.preview.Preview
+import eu.europa.ec.uilogic.component.preview.ThemeModePreviews
+import eu.europa.ec.uilogic.component.utils.SPACING_LARGE
+import eu.europa.ec.uilogic.component.utils.measureTextWidthInPx
+import kotlinx.coroutines.CoroutineScope
 
+private val horizontalLabelPaddingDp = SPACING_SMALL_PLUS.dp
 private val paddingValues = PaddingValues(
-    horizontal = SPACING_SMALL_PLUS.dp, vertical = SPACING_EXTRA_SMALL.dp
+    horizontal = horizontalLabelPaddingDp, vertical = SPACING_EXTRA_SMALL.dp
 )
+private val labelTextStyle: TextStyle
+    @Composable
+    get() = MaterialTheme.typography.titleSmall
 
 private const val inactiveTextColorAlpha = 0.35f
 
 @Composable
 fun WrapStepBar(currentStep: Int, steps: List<String>, modifier: Modifier = Modifier) {
+    val textMeasurer = rememberTextMeasurer()
+    val listState = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
+
+    val startIndex = calculateStartIndex(currentStep, steps, textMeasurer)
+
     ElevatedCard(
-        modifier = modifier
-            .fillMaxWidth(),
+        modifier = modifier.fillMaxWidth(),
         shape = buttonsShape,
         elevation = CardDefaults.elevatedCardElevation(defaultElevation = SIZE_EXTRA_SMALL.dp),
         colors = CardDefaults.elevatedCardColors(
             MaterialTheme.colorScheme.background,
         ),
     ) {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
+        LazyRow(
+            state = listState,
+            modifier = Modifier
+                .fillMaxWidth()
+                .clipToBounds(),
             verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween,
+            horizontalArrangement = Arrangement.Start,
+            userScrollEnabled = true
         ) {
-            steps.forEachIndexed { index, text ->
-                Label(text, index, currentStep)
+            itemsIndexed(steps) { index, text ->
+                Label(
+                    text = text,
+                    index = index,
+                    currentStep = currentStep
+                )
             }
         }
     }
+
+    scrollToTheStartIndex(startIndex, coroutineScope, listState)
 }
+
+@Composable
+private fun scrollToTheStartIndex(
+    startIndex: Int,
+    coroutineScope: CoroutineScope,
+    listState: LazyListState
+) {
+    LaunchedEffect(startIndex) {
+        coroutineScope.launch {
+            listState.scrollToItem(startIndex)
+        }
+    }
+}
+
+@Composable
+private fun calculateStartIndex(
+    currentStep: Int,
+    steps: List<String>,
+    textMeasurer: TextMeasurer
+): Int {
+    if (currentStep == 0) {
+        return 0
+    } else if (currentStep < steps.size - 2) {
+        return currentStep - 1
+    }
+
+    // only for the last two steps we calculate the start index in a fancy way
+    // taking into account the screen and font size
+    var startIndex = calculateStartIndexForTrailingItems(currentStep, steps, textMeasurer)
+
+    return startIndex
+}
+
+@Composable
+private fun calculateStartIndexForTrailingItems(
+    currentStep: Int,
+    steps: List<String>,
+    textMeasurer: TextMeasurer
+): Int {
+    val density = LocalDensity.current
+    val fontScale = density.fontScale
+    val configuration = LocalConfiguration.current
+    val rowWidthPx = with(density) { (configuration.screenWidthDp - 2 * SPACING_LARGE).dp.toPx() }
+    val paddingWidthPx = with(density) { 2 * horizontalLabelPaddingDp.toPx() }
+
+    var totalWidth = 0f
+    var startIndex = currentStep
+    for (i in steps.size - 1 downTo 0) {
+        val textWidth = measureTextWidthInPx(steps[i], textMeasurer, labelTextStyle, fontScale)
+        val stepWidth = textWidth + paddingWidthPx
+        if (totalWidth + stepWidth > rowWidthPx) {
+            break
+        }
+        totalWidth += stepWidth
+        startIndex = i
+    }
+    return startIndex
+}
+
+
 
 @Composable
 private fun Label(text: String, index: Int, currentStep: Int) {
@@ -74,7 +169,7 @@ private fun Label(text: String, index: Int, currentStep: Int) {
         modifier = Modifier.padding(paddingValues),
         text = text,
         color = textColor,
-        style = MaterialTheme.typography.titleSmall,
+        style = labelTextStyle,
         maxLines = 1,
         overflow = TextOverflow.Ellipsis,
     )
@@ -91,7 +186,10 @@ fun getColor(isActive: Boolean, inactiveAlpha: Float) =
 fun StartupProgressBarPreview() {
     PreviewTheme {
         WrapStepBar(
-            currentStep = 1, steps = listOf("Step 1", "Step 2", "Step 3")
+            currentStep = 5,
+            steps = listOf("Step 1", "Step 2", "Step 3", "Step 4", "Step 5", "Step 6")
         )
     }
 }
+
+
